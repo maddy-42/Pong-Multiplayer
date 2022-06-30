06653d8840f79d37409f695bb922e2db7a13117f
Himanshu <107607846+violetdrago@users.noreply.github.com>
2022-06-30 12:32:40 +0530

    added a clock to maintain game fps

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import pygame
-
+pygame.init()
 
 WIDTH, HEIGHT = 700, 500
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Pong")
 
+FPS = 60
+
 def main():
     run = True
+    clock = pygame.tick.Clock()
 
     while run:
+
+        clock.tick(FPS)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
